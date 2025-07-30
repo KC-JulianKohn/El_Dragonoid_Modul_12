3543dec1ae534d7903d4231f2b350b53670d9688
Julian Kohn <julian.kohn@hotmail.de>
2025-07-30 21:49:18 +0200

    add fixed damage charakter animation

--- scripts/models/movable-object.class.js
+++ scripts/models/movable-object.class.js
@@ -59,7 +59,7 @@ class MovableObject extends DrawableObject {
     isHurt() {
         let timepassed = new Date().getTime() - this.lastHit;
         timepassed = timepassed / 1000;
-        return timepassed < 2;
+        return timepassed < 1;
     }
 
     isDead() {
